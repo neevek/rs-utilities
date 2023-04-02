16a33618b3000f13171fe1d848163a736056b365
neevek <i@neevek.net>
2023-04-02 17:29:59 +0800

    add unwrap_or_return

--- src/macros.rs
+++ src/macros.rs
@@ -16,3 +16,13 @@ macro_rules! unwrap_or_continue {
         }
     };
 }
+
+#[macro_export]
+macro_rules! unwrap_or_return {
+    ($e:expr, $r:expr) => {
+        match $e {
+            Some(e) => e,
+            None => return $r,
+        }
+    };
+}
